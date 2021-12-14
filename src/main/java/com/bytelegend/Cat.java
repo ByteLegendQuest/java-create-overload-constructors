c0aa2a3bf3a820783a6e7895775b7ab13269d95a
jaryarbn <a15146980388@163.com>
2021-12-14 05:17:14 +0000

    Update at 2021-12-14T05:17:14.113714Z

--- src/main/java/com/bytelegend/Cat.java
+++ src/main/java/com/bytelegend/Cat.java
@@ -6,7 +6,7 @@ public class Cat {
         this.name = name;
     }
     public Cat(){
-        this.("");
+        this("");
     }
     public static void main(String[] args) {
         System.out.println(new Cat(""));

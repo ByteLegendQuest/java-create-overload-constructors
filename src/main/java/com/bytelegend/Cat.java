35d1d29ac5f1b9d20a6989dc22e7b628295ed39c
CaptainOfPhB <captainofphb@gmail.com>
2021-12-15 11:17:46 +0800

    Update Cat.java

--- src/main/java/com/bytelegend/Cat.java
+++ src/main/java/com/bytelegend/Cat.java
@@ -2,6 +2,10 @@ package com.bytelegend;
 
 public class Cat {
     public String name;
+    
+    Cat(String name) {
+        this.name = name;
+    }
 
     public static void main(String[] args) {
         System.out.println(new Cat(""));

4cc2039d6ea1d59fc6682f855f8c0183aa13bae8
MeteorAndy <AndyLamwxl@Outlook.com>
2021-12-28 01:26:45 +0000

    Update at 2021-12-28T01:26:45.128238Z

--- src/main/java/com/bytelegend/Cat.java
+++ src/main/java/com/bytelegend/Cat.java
@@ -12,4 +12,12 @@ public class Cat {
     public String toString() {
         return "Cat(" + name + ")";
     }
+
+    public Cat(string name){
+        this.name=name;
+    }
+
+    public Cat(){
+        this("");
+    }
 }

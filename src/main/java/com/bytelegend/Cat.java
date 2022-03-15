ac10bd8e2a9be847b143697d0caba4d69c4da947
lemon-altxx <2515782282@qq.com>
2022-03-15 09:48:19 +0000

    Update at 2022-03-15T09:48:19.780721Z

--- src/main/java/com/bytelegend/Cat.java
+++ src/main/java/com/bytelegend/Cat.java
@@ -7,7 +7,7 @@ public class Cat {
         this.name = name;
     }
     public Cat() {
-        Cat("");
+        this("");
     }
     public static void main(String[] args) {
         System.out.println(new Cat(""));

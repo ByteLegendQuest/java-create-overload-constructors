2e677110aa71b6229b14a2a8635a4a1df466cec6
Lh549 <1606953936@qq.com>
2022-03-17 06:11:32 +0000

    Update at 2022-03-17T06:11:32.894900Z

--- src/main/java/com/bytelegend/Cat.java
+++ src/main/java/com/bytelegend/Cat.java
@@ -8,6 +8,12 @@ public class Cat {
         System.out.println(new Cat("White"));
     }
 
+    public Cat() { }
+
+    public Cat(String name) {
+        this.name = name;
+    }
+
     @Override
     public String toString() {
         return "Cat(" + name + ")";

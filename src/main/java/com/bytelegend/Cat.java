cdd06b78fb57c53e04fb47a854d1e6c7e7686d2d
yunghf <yung.hf@outlook.com>
2022-03-24 13:05:24 +0000

    Update at 2022-03-24T13:05:24.204011Z

--- src/main/java/com/bytelegend/Cat.java
+++ src/main/java/com/bytelegend/Cat.java
@@ -3,6 +3,15 @@ package com.bytelegend;
 public class Cat {
     public String name;
 
+    public Cat(String name){
+        this.name = name;
+        return this;
+    }
+
+    public Cat(){
+        return Cat("");
+    }
+
     public static void main(String[] args) {
         System.out.println(new Cat(""));
         System.out.println(new Cat("White"));

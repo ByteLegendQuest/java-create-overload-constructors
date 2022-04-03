13591d54ad07c1148cc684c91a67ee0f9e1422c5
denghaoze <57385221+denghaoze@users.noreply.github.com>
2022-04-03 11:56:31 +0000

    Update at 2022-04-03T11:56:31.515513Z

--- src/main/java/com/bytelegend/Cat.java
+++ src/main/java/com/bytelegend/Cat.java
@@ -3,12 +3,13 @@ package com.bytelegend;
 public class Cat {
     public String name;
 
-    public Cat() {
-    }
+   public Cat(){
+       new Cat("");
+   }
 
-    public Cat(String name) {
-        this.name = name;
-    }
+   public Cat(String name){
+       this.name = name;
+   }
 
     public static void main(String[] args) {
         System.out.println(new Cat(""));

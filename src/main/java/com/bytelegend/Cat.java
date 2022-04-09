d86ca2904129b97bf0462072dd05b27f6bd309c3
liubaolai <1164552059@qq.com>
2022-04-09 12:39:23 +0000

    Update at 2022-04-09T12:39:23.501607Z

--- src/main/java/com/bytelegend/Cat.java
+++ src/main/java/com/bytelegend/Cat.java
@@ -12,4 +12,11 @@ public class Cat {
     public String toString() {
         return "Cat(" + name + ")";
     }
+
+     public Cat() {
+        this("");
+    }
+    public Cat(String name) {
+        this.name = name;
+    }
 }

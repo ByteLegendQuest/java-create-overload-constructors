3189a4752e74fbf7e90fce8f053c1d4b005e0cf3
KFig21 <Kfig21@gmail.com>
2022-04-29 20:35:04 +0000

    Update at 2022-04-29T20:35:03.999711Z

--- src/main/java/com/bytelegend/Cat.java
+++ src/main/java/com/bytelegend/Cat.java
@@ -3,8 +3,15 @@ package com.bytelegend;
 public class Cat {
     public String name;
 
+    public Cat(String name){
+        this.name = name;
+    }
+    public Cat(){
+        new Cat("");
+    }
+
     public static void main(String[] args) {
-        System.out.println(new Cat(""));
+        System.out.println(new Cat("Black"));
         System.out.println(new Cat("White"));
     }
 

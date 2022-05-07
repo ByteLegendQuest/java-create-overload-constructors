a6a5689e9d94fef2fa32f661f5e60a1869c95ce2
kangming06 <kangming06@hotmail.com>
2022-05-07 08:00:51 +0000

    Update at 2022-05-07T08:00:51.707944Z

--- src/main/java/com/bytelegend/Cat.java
+++ src/main/java/com/bytelegend/Cat.java
@@ -2,7 +2,9 @@ package com.bytelegend;
 
 public class Cat {
     public String name;
-
+    public Cat(String... name) {
+        self.name = name[0];
+    }
     public static void main(String[] args) {
         System.out.println(new Cat(""));
         System.out.println(new Cat("White"));

d025042a116519aa5f9ce9a6e086664240c70ebd
kangming06 <kangming06@hotmail.com>
2022-05-07 08:01:48 +0000

    Update at 2022-05-07T08:01:48.645979Z

--- src/main/java/com/bytelegend/Cat.java
+++ src/main/java/com/bytelegend/Cat.java
@@ -3,7 +3,9 @@ package com.bytelegend;
 public class Cat {
     public String name;
     public Cat(String... name) {
-        self.name = name[0];
+        if (name.length > 0) {
+            self.name = name[0];
+        }
     }
     public static void main(String[] args) {
         System.out.println(new Cat(""));

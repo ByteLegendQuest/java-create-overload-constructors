8d75e616652dac8f90f356016271928f1ed7227d
Yeely0162 <Yeely01621@163.com>
2022-05-11 07:58:18 +0000

    Update at 2022-05-11T07:58:18.390699Z

--- src/main/java/com/bytelegend/Cat.java
+++ src/main/java/com/bytelegend/Cat.java
@@ -3,6 +3,11 @@ package com.bytelegend;
 public class Cat {
     public String name;
 
+    public Cat (String name){
+        Cat c = new Cat();
+        c.name=name;
+        return c;
+    }
     public static void main(String[] args) {
         System.out.println(new Cat(""));
         System.out.println(new Cat("White"));

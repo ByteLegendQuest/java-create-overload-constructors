fd7662b3ab3bb30667d95499ed9b41f110e57ef2
Yeely0162 <Yeely01621@163.com>
2022-05-11 08:17:16 +0000

    Update at 2022-05-11T08:17:16.808981Z

--- src/main/java/com/bytelegend/Cat.java
+++ src/main/java/com/bytelegend/Cat.java
@@ -3,11 +3,11 @@ package com.bytelegend;
 public class Cat {
     public String name;
 
+    public Cat() {
+        this("");
+    }
     public Cat (String name){
         this.name = name;
-    }
-    public Cat () {
-        new Cat("");
     }
     public static void main(String[] args) {
         System.out.println(new Cat(""));

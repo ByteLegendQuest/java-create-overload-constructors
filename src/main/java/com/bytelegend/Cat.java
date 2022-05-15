d7c8900821f915f0a438188b2c4ba437f861d16a
baixiangyang <443771196@qq.com>
2022-05-15 08:57:11 +0000

    Update at 2022-05-15T08:57:11.700083Z

--- src/main/java/com/bytelegend/Cat.java
+++ src/main/java/com/bytelegend/Cat.java
@@ -2,7 +2,14 @@ package com.bytelegend;
 
 public class Cat {
     public String name;
-
+    
+    public Cat(){
+        this("");
+    }
+    
+    public Cat(String name){
+        this.name = name;
+    }
     public static void main(String[] args) {
         System.out.println(new Cat(""));
         System.out.println(new Cat("White"));

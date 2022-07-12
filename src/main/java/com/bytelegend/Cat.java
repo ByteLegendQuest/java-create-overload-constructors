5c7bb61688432498b40bb0823534aa6c2560b958
Birlinha <omenaribeiro13@gmail.com>
2022-07-12 02:34:59 +0000

    Update at 2022-07-12T02:34:59.636217Z

--- src/main/java/com/bytelegend/Cat.java
+++ src/main/java/com/bytelegend/Cat.java
@@ -3,6 +3,15 @@ package com.bytelegend;
 public class Cat {
     public String name;
 
+    Cat(String str){
+        name = str;
+    }
+
+    Cat() {
+        Cat("");
+    }
+
+
     public static void main(String[] args) {
         System.out.println(new Cat(""));
         System.out.println(new Cat("White"));

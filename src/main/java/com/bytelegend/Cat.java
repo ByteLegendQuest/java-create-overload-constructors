20b44906df1dd378c82c00fff534298962b8c92c
DescentDD <1740811552@qq.com>
2022-07-16 12:12:11 +0000

    Update at 2022-07-16T12:12:11.639183Z

--- src/main/java/com/bytelegend/Cat.java
+++ src/main/java/com/bytelegend/Cat.java
@@ -7,7 +7,7 @@ public class Cat {
         System.out.println(new Cat(""));
         System.out.println(new Cat("White"));
      }
-    public Cat(String name) {
+    public Cat(String name){
         this.name = name;
      }
     public Cat() {
@@ -15,7 +15,7 @@ public class Cat {
      
 
     @Override
-    public String toString() {
+    public String toString(){
         return "Cat(" + name + ")";
     }
 }

b2fc69500b35ef743b61422ee934e44c22ffaf07
TOHSAKA-RINN <1911872724@qq.com>
2022-07-20 13:05:09 +0000

    Update at 2022-07-20T13:05:09.806970Z

--- src/main/java/com/bytelegend/Cat.java
+++ src/main/java/com/bytelegend/Cat.java
@@ -2,7 +2,10 @@ package com.bytelegend;
 
 public class Cat {
     public String name;
-
+    public Cat () {}
+    public Cat (String name) {
+        this.name=name;
+    }
     public static void main(String[] args) {
         System.out.println(new Cat(""));
         System.out.println(new Cat("White"));

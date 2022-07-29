6b156b8c06c275cd28890966f5775f42c136f032
TaoYang1998 <1121758475@qq.com>
2022-07-29 15:44:16 +0000

    Update at 2022-07-29T15:44:16.307208Z

--- src/main/java/com/bytelegend/Cat.java
+++ src/main/java/com/bytelegend/Cat.java
@@ -8,6 +8,13 @@ public class Cat {
         System.out.println(new Cat("White"));
     }
 
+    public Cat() {
+    }
+
+    public Cat(String name) {
+        this.name = name;
+    }
+
     @Override
     public String toString() {
         return "Cat(" + name + ")";

5cbf9f154e6898263ec4e2bed2099f775110b193
guda04021 <1349308535@qq.com>
2022-11-10 12:45:19 +0000

    Update at 2022-11-10T12:45:19.048145Z

--- src/main/java/com/bytelegend/Cat.java
+++ src/main/java/com/bytelegend/Cat.java
@@ -3,6 +3,15 @@ package com.bytelegend;
 public class Cat {
     public String name;
 
+    public Cat(String white) {
+        name = white;
+    }
+
+
+    public Cat Cat(String white) {
+        return new Cat("");
+    }
+
     public static void main(String[] args) {
         System.out.println(new Cat(""));
         System.out.println(new Cat("White"));

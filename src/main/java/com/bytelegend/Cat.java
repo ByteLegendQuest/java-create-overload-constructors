aa756342b6db540ed4490b6991866ce3765dbb32
Newelle <wozi1988@proton.me>
2022-11-14 03:53:05 +0000

    Update at 2022-11-14T03:53:05.321734Z

--- src/main/java/com/bytelegend/Cat.java
+++ src/main/java/com/bytelegend/Cat.java
@@ -3,6 +3,13 @@ package com.bytelegend;
 public class Cat {
     public String name;
 
+    Cat(){
+        this.name = "";
+    }
+    Cat(String name){
+        this.name = name;
+    }
+
     public static void main(String[] args) {
         System.out.println(new Cat(""));
         System.out.println(new Cat("White"));

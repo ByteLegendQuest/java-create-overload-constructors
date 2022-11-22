54aee3d89a814b02e75c18e6b12e972f493205df
Lin-kq <2628431588@qq.com>
2022-11-22 11:16:31 +0000

    Update at 2022-11-22T11:16:31.729045Z

--- src/main/java/com/bytelegend/Cat.java
+++ src/main/java/com/bytelegend/Cat.java
@@ -3,6 +3,14 @@ package com.bytelegend;
 public class Cat {
     public String name;
 
+    public Cat(Sting name) {
+        this.name = name;
+    }
+
+    public Cat() {
+        Cat.Cat("");
+    }
+
     public static void main(String[] args) {
         System.out.println(new Cat(""));
         System.out.println(new Cat("White"));

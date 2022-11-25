9062bb1d0a57f05b5425e15f96d19a86056b26f0
abmcar <abmcar@qq.com>
2022-11-25 05:39:44 +0000

    Update at 2022-11-25T05:39:44.969931Z

--- src/main/java/com/bytelegend/Cat.java
+++ src/main/java/com/bytelegend/Cat.java
@@ -8,6 +8,14 @@ public class Cat {
         System.out.println(new Cat("White"));
     }
 
+    Cat(String name) {
+        this.name = name;
+    }
+
+    Cat() {
+        Cat("");
+    }
+
     @Override
     public String toString() {
         return "Cat(" + name + ")";

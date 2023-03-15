69e1c3a8241c247ffc1a09fcdd57d6d2a5fed86e
an5er <1318861442@qq.com>
2023-03-15 11:22:50 +0000

    Update at 2023-03-15T11:22:50.850978Z

--- src/main/java/com/bytelegend/Cat.java
+++ src/main/java/com/bytelegend/Cat.java
@@ -2,6 +2,12 @@ package com.bytelegend;
 
 public class Cat {
     public String name;
+    public Cat(){
+        Cat("");
+    }
+    public Cat(String name){
+        this.name = name;
+    }
 
     public static void main(String[] args) {
         System.out.println(new Cat(""));

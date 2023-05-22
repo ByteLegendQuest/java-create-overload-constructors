21ddb8b46c47260ff032b8d092c0eca0ad42444f
NahuelBullonJensen <nahuelbullonjensen@gmail.com>
2023-05-22 00:31:30 +0000

    Update at 2023-05-22T00:31:30.134036Z

--- src/main/java/com/bytelegend/Cat.java
+++ src/main/java/com/bytelegend/Cat.java
@@ -3,6 +3,15 @@ package com.bytelegend;
 public class Cat {
     public String name;
 
+    
+    public Cat() {
+        this.name = "";
+    }
+
+    public Cat(String name) {
+        this.name;
+    }
+
     public static void main(String[] args) {
         System.out.println(new Cat(""));
         System.out.println(new Cat("White"));
